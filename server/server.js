2675c348b59024c48719f3384806c1c81f3d254f
jeyachandranj <j.jeyachandran072@gmail.com>
2024-08-18 10:18:10 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/translator', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://jeyachandranj:jj.jeyan@cluster0.pe8ib.mongodb.net/translate', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
